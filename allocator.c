50082e61dac6953b482b9ad1f525bcd20e665bf0
aban-ali <22f1001086@ds.study.iitm.ac.in>
2026-04-30 17:55:24 +0000

    memory allocation done, working on memory freeing

---
+++ allocator.c
@@ -0,0 +1,57 @@
+#include "allocator.h"
+
+static char memory_pool[MEMORY_SIZE];
+static block_t* free_list = NULL;
+
+void init_allocator(){
+    free_list = (block_t*) memory_pool;
+    free_list->size = MEMORY_SIZE - sizeof(block_t);
+    free_list->free = 1;
+    free_list->next = NULL;
+}
+
+// void init_block(block_t* block){
+//     block->free = 1;
+//     block->next = NULL;
+//     block->size = MEMORY_SIZE - (free_list - block) - sizeof(block_t);
+//     return;
+// }
+
+
+void* my_malloc(size_t size){
+    block_t *curr = free_list;
+
+    while(curr){
+        if(curr->free && curr->size >= size){
+            if(curr->size >= sizeof(block_t) + size + 1){
+
+                block_t* block = (block_t*)((char*)curr + sizeof(block_t) + size);
+
+                block->size = curr->size - size - sizeof(block_t);
+                block->free = 1;
+                block->next = curr->next;
+
+                curr->size = size;
+                curr->next = block;
+            }
+            curr->free = 0;
+            return (void*)(curr + 1);
+        }
+        curr = curr->next;
+    }
+    return NULL;
+}
+
+
+void my_free(void* ptr){
+    if(!ptr) return;
+
+    block_t* block = (block_t*)ptr - 1;
+    block->free = 1;
+    block_t* curr = block->next;
+    while(curr && curr->free){
+        block->size += curr->size + sizeof(block_t);
+        block->next = curr->next;
+        curr = curr->next;
+    }
+}
